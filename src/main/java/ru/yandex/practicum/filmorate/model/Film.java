2ef9251bedb67295acee6c4035845c939368164c
Vlad <ma1ngeek@yandex.ru>
2023-05-27 19:42:06 +0300

    1

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Film {
+    private long id;
+    private @NotNull
+    @NotEmpty String name;
+    @Size(min = 1, max = 200, message = "Описание фильма не должно превышать 200 символов.")
+    private String description;
+    private @NotNull LocalDate releaseDate;
+    @Positive(message = "Продолжительность не может быть отрицательной")
+    private int duration;
+
+}
+
