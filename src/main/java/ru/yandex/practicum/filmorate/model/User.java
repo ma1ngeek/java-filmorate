76bef2628cd17622b5da0c72dc12afde559a20a1
Vlad <ma1ngeek@yandex.ru>
2023-05-28 23:25:45 +0300

    1

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private long id;
+    private @NotNull
+    @NotEmpty
+    @Email String email;
+    private @NotNull
+    @NotEmpty String login;
+    private String name;
+    @Past
+    private LocalDate birthday;
+
+}
